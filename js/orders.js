3a8aa56e5403afb11a572fa10cd9aecc5f8b9a18
PeterRamsis <peterzizo100@gmail.com>
2024-02-12 00:33:03 +0200

    last change after add new users

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+ {
+    productName: "Javascript" ,
+    productNumber : '34343',
+    paymentStatus: 'Due',
+    status: 'Pending'
+ },
+ {
+    productName: 'CSS Full Course',
+    productNumber: '97245',
+    paymentStatus: 'Refunded',
+    status: 'Declined'
+},
+{
+    productName: 'Flex-Box Tutorial',
+    productNumber: '36452',
+    paymentStatus: 'Paid',
+    status: 'Active'
+},
+];
